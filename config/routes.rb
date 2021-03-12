9c5da092486d944f7406e54e6748e36d9f22f3ce
shouko.yamashita <white_lemontea@yahoo.co.jp>
2021-03-12 11:22:43 +0000

    post

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,7 @@ Rails.application.routes.draw do
   
   get 'signup', to: 'users#new'
   resources :users,only: [:index, :show, :create]
+  
+  resources :microposts, only: [:create, :destroy]
+
 end
